f39525decdeec0a2988c04c0221afd26afd6bbae
john argote <argote.john@gmail.com>
2018-07-04 00:11:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine,name) {
+  katzDeliLine.push(name);
+  var position = katzDeliLine.length;
+  return `Welcome, ${name}. You are number ${position} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  var i = katzDeliLine.length;
+  var count = 0;
+  if(count < i) {
+    return (`Currently serving ${katzDeliLine.shift()}.`);
+  }
+  else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var name = katzDeliLine;
+  var i = katzDeliLine.length;
+  var count = 0;
+  while (count < i) {
+    count++;
+    return `The line is currently: 1. ${name[0]}.`;
+  }
+  return "The line is currently empty.";
+}
