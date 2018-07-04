bda5b71e093251a4295ad27a6e70bca65a1afef3
john argote <argote.john@gmail.com>
2018-07-04 01:23:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,21 +17,12 @@ function nowServing(katzDeliLine) {
   }
 }
 
-
 function currentLine(katzDeliLine) {
   var line = [];
+  var i = 0;
   
-  let i = 0;
-  while (i < katzDeliLine.length) {
-    line.push(+ [i + 1] + `. ` + katzDeliLine[i])
-    i++;
+  while(i<katzDeliLine.length) {
+    line.push(' ' +[i + 1]+ '. ' +katzDeliLine[i]);
   }
   
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty.";
-  } else {
-    return (`The line is currently: ` + line);
-  }
 }
-
-
